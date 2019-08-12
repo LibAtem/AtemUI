71d065fba3bdfaa3a7cf4ccf67a38400c424c7e9
Julian Waller <git@julusian.co.uk>
2019-08-12 19:35:35 +0100

    chore: Replace client with fresh react-app boilerplate

--- AtemServer/ClientApp/src/App.js
+++ AtemServer/ClientApp/src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends Component {
-  displayName = App.name
-
-  render() {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
