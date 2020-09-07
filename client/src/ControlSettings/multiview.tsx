79a83aacc278850d1d3b8466a975775f79404138
Julian Waller <git@julusian.co.uk>
2020-09-08 00:44:21 +0100

    feat: more multiview config work

--- client/src/ControlSettings/multiview.tsx
+++ client/src/ControlSettings/multiview.tsx
@@ -1,19 +1,12 @@
-import { Container, Row, ButtonGroup, Button } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
 import React from 'react'
-import multiview1 from './assets/multiview1.svg'
-import multiview2 from './assets/multiview2.svg'
-import multiview3 from './assets/multiview3.svg'
-import multiview4 from './assets/multiview4.svg'
 import { StickyPanelBase } from '../Control/Settings/base'
 import { SendCommandStrict } from '../device-page-wrapper'
-import { LibAtemState, LibAtemEnums } from '../generated'
+import { LibAtemState, LibAtemEnums, LibAtemCommands } from '../generated'
 import { SourcesMap, SourceSelectInput, AtemButtonBar } from '../components'
-
-// class MvButton extends React.Component {
-//     render() {
-//         return <Button />
-//     }
-// }
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { LayoutIcon } from './multiviewLayout'
 
 interface MultiViewSettingsProps {
   sendCommand: SendCommandStrict
@@ -49,11 +42,12 @@ export class MultiViewSettings extends StickyPanelBase<MultiViewSettingsProps, M
     const window = mv.windows[index]
     const name = this.props.sources.get(window.source)?.longName ?? ''
 
-    // const isLeft = index === 0
-    // const isPreview = isLeft !== mv.properties.programPreviewSwapped
-    // mv.win
-
-    return <div className="mutliViewItem programPreview">{name}</div>
+    return (
+      <div className="mutliViewItem programPreview">
+        <div className="name-row">{name}</div>
+        {this.renderBoxButton(mv, index)}
+      </div>
+    )
   }
 
   private renderSmallGrid(
@@ -62,58 +56,191 @@ export class MultiViewSettings extends StickyPanelBase<MultiViewSettingsProps, M
     boxIds: [number, number, number, number]
   ) {
     const mvIndex = this.state.page
+    return <div className="multiViewSubGrid">{boxIds.map((i) => this.renderWindowBox(info, mv, i))}</div>
+  }
+
+  private renderWindowBox(
+    info: LibAtemState.InfoState_MultiViewInfoState,
+    mv: LibAtemState.MultiViewerState,
+    boxId: number
+  ) {
+    const win = mv.windows[boxId]
     return (
-      <div className="multiViewSubGrid">
-        {boxIds.map((i) => (
-          <div key={i} className="mutliViewItem">
-            {info.canRouteInputs ? (
-              <SourceSelectInput
-                label={null}
-                sources={this.props.sources}
-                sourceAvailability={LibAtemEnums.SourceAvailability.Multiviewer}
-                value={mv.windows[i].source}
-                onChange={(v) => {
-                  this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewWindowInputSetCommand', {
-                    MultiviewIndex: mvIndex,
-                    WindowIndex: i,
-                    Source: v,
-                  })
-                }}
-              />
-            ) : (
-              this.props.sources.get(mv.windows[i].source)?.longName ?? ''
-            )}
-          </div>
-        ))}
+      <div key={boxId} className="mutliViewItem">
+        <div>
+          {info.canRouteInputs ? (
+            <SourceSelectInput
+              label={null}
+              sources={this.props.sources}
+              sourceAvailability={LibAtemEnums.SourceAvailability.Multiviewer}
+              value={win.source}
+              onChange={(v) => {
+                this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewWindowInputSetCommand', {
+                  MultiviewIndex: this.state.page,
+                  WindowIndex: boxId,
+                  Source: v,
+                })
+              }}
+            />
+          ) : (
+            this.props.sources.get(win.source)?.longName ?? ''
+          )}
+        </div>
+        {this.renderBoxButton(mv, boxId)}
+      </div>
+    )
+  }
+
+  private renderBoxButton(mv: LibAtemState.MultiViewerState, box: number) {
+    const boxProps = mv.windows[box]
+
+    return (
+      <div className="button-row">
+        {boxProps?.supportsSafeArea ? (
+          <Button
+            bsPrefix="multiViewButton"
+            variant={boxProps.safeAreaEnabled ? 'primary' : 'secondary'}
+            onClick={() => {
+              this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewWindowSafeAreaCommand', {
+                MultiviewIndex: this.state.page,
+                WindowIndex: box,
+                SafeAreaEnabled: !boxProps.safeAreaEnabled,
+              })
+            }}
+          >
+            <SafeAreaIcon isCurrent={boxProps.safeAreaEnabled} />
+          </Button>
+        ) : (
+          ''
+        )}
+        {boxProps?.supportsVuMeter ? (
+          <Button
+            bsPrefix="multiViewButton"
+            variant={boxProps.vuMeterEnabled ? 'primary' : 'secondary'}
+            onClick={() => {
+              this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewWindowVuMeterSetCommand', {
+                MultiviewIndex: this.state.page,
+                WindowIndex: box,
+                VuEnabled: !boxProps.vuMeterEnabled,
+              })
+            }}
+          >
+            <VuMeterIcon isCurrent={boxProps.vuMeterEnabled} />
+          </Button>
+        ) : (
+          ''
+        )}
+      </div>
+    )
+  }
+
+  private renderSwapButton(mv: LibAtemState.MultiViewerState) {
+    return (
+      <div className="swap-preview-program">
+        <Button
+          bsPrefix="multiViewButton"
+          variant="secondary"
+          onClick={() => {
+            this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewPropertiesSetV8Command', {
+              MultiviewIndex: this.state.page,
+              Mask: LibAtemCommands.Settings_Multiview_MultiviewPropertiesSetV8Command_MaskFlags.ProgramPreviewSwapped,
+              ProgramPreviewSwapped: !mv.properties.programPreviewSwapped,
+            })
+          }}
+        >
+          <FontAwesomeIcon icon={faSyncAlt} />
+        </Button>
       </div>
     )
   }
 
   private renderContent(info: LibAtemState.InfoState_MultiViewInfoState, mv: LibAtemState.MultiViewerState) {
-    if (info.supportsQuadrantLayout) {
-      return <div>TODO</div>
-    } else {
-      var layout = [] as number[][]
-      if (mv.properties.layout == 3) {
-        layout = [[2, 3, 6, 7], [4, 5, 8, 9], [0], [1]]
-      } else if (mv.properties.layout == 5) {
-        layout = [[2, 3, 4, 5], [0], [6, 7, 8, 9], [1]]
-      } else if (mv.properties.layout == 10) {
-        layout = [[0], [2, 3, 4, 5], [1], [6, 7, 8, 9]]
-      } else if (mv.properties.layout == 12) {
-        layout = [[0], [1], [2, 3, 6, 7], [4, 5, 8, 9]]
-      }
-      var content = []
-      for (var i = 0; i < layout.length; i++) {
-        if (layout[i].length == 1) {
-          content.push(this.renderLegacyLargeWindow(mv, layout[i][0]))
-        } else {
-          content.push(this.renderSmallGrid(info, mv, layout[i] as any))
-        }
-      }
+    var layout = [] as Array<[number, number, number, number] | number>
 
-      return content
+    const current = mv.properties.layout
+    if (info.supportsQuadrantLayout) {
+      layout = [
+        (current & LibAtemEnums.MultiViewLayoutV8.TopLeftSmall) > 0 ? [0, 1, 4, 6] : 0,
+        (current & LibAtemEnums.MultiViewLayoutV8.TopRightSmall) > 0 ? [2, 3, 6, 7] : 2,
+        (current & LibAtemEnums.MultiViewLayoutV8.BottomLeftSmall) > 0 ? [8, 9, 12, 13] : 8,
+        (current & LibAtemEnums.MultiViewLayoutV8.BottomRightSmall) > 0 ? [10, 11, 14, 15] : 10,
+      ]
+    } else {
+      switch (current) {
+        case LibAtemEnums.MultiViewLayoutV8.ProgramTop:
+          layout = [0, 1, [2, 3, 6, 7], [4, 5, 8, 9]]
+          break
+        case LibAtemEnums.MultiViewLayoutV8.ProgramBottom:
+          layout = [[2, 3, 6, 7], [4, 5, 8, 9], 0, 1]
+          break
+        case LibAtemEnums.MultiViewLayoutV8.ProgramLeft:
+          layout = [0, [2, 3, 4, 5], 1, [6, 7, 8, 9]]
+          break
+        case LibAtemEnums.MultiViewLayoutV8.ProgramRight:
+          layout = [[2, 3, 4, 5], 0, [6, 7, 8, 9], 1]
+          break
+      }
     }
+
+    const content = layout.map((inf) => {
+      if (typeof inf === 'number') {
+        if (info.supportsQuadrantLayout) {
+          return this.renderWindowBox(info, mv, inf)
+        } else {
+          return this.renderLegacyLargeWindow(mv, inf)
+        }
+      } else {
+        return this.renderSmallGrid(info, mv, inf)
+      }
+    })
+
+    if (info.supportsProgramPreviewSwapped) {
+      content.push(this.renderSwapButton(mv))
+    }
+
+    return content
+  }
+
+  private renderChangeLayout(
+    info: LibAtemState.InfoState_MultiViewInfoState,
+    mv: LibAtemState.MultiViewerState_PropertiesState
+  ) {
+    if (info.supportsQuadrantLayout) {
+      return <div className="layout-bar">TODO</div>
+    } else {
+      return (
+        <div className="layout-bar">
+          <LayoutButton
+            sendCommand={this.props.sendCommand}
+            mvIndex={this.state.page}
+            current={mv.layout}
+            target={LibAtemEnums.MultiViewLayoutV8.ProgramTop}
+          />
+          <LayoutButton
+            sendCommand={this.props.sendCommand}
+            mvIndex={this.state.page}
+            current={mv.layout}
+            target={LibAtemEnums.MultiViewLayoutV8.ProgramBottom}
+          />
+          <LayoutButton
+            sendCommand={this.props.sendCommand}
+            mvIndex={this.state.page}
+            current={mv.layout}
+            target={LibAtemEnums.MultiViewLayoutV8.ProgramLeft}
+          />
+          <LayoutButton
+            sendCommand={this.props.sendCommand}
+            mvIndex={this.state.page}
+            current={mv.layout}
+            target={LibAtemEnums.MultiViewLayoutV8.ProgramRight}
+          />
+        </div>
+      )
+    }
+  }
+
+  private renderVuControls(mv: LibAtemState.MultiViewerState_PropertiesState) {
+    return ''
   }
 
   render() {
@@ -126,7 +253,7 @@ export class MultiViewSettings extends StickyPanelBase<MultiViewSettingsProps, M
     }
 
     return (
-      <Container className="maxW">
+      <Container className="maxW" style={{ paddingTop: '1rem' }}>
         {this.props.multiViewers.length > 1 || this.state.page !== 0 ? (
           <AtemButtonBar
             selected={this.state.page}
@@ -139,64 +266,135 @@ export class MultiViewSettings extends StickyPanelBase<MultiViewSettingsProps, M
         ) : (
           ''
         )}
-        {info.canChangeLayout ? (
-          <Row className="justify-content-md-center p-5">
-            <ButtonGroup aria-label="Basic example">
-              <Button
-                variant={mv.properties.layout == 12 ? 'primary' : 'secondary'}
-                onClick={() =>
-                  this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewPropertiesSetV8Command', {
-                    MultiviewIndex: mvIndex,
-                    Mask: 1,
-                    Layout: 12,
-                  })
-                }
-              >
-                <img src={multiview1} alt="1" />
-              </Button>
-              <Button
-                variant={mv.properties.layout == 3 ? 'primary' : 'secondary'}
-                onClick={() =>
-                  this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewPropertiesSetV8Command', {
-                    MultiviewIndex: mvIndex,
-                    Mask: 1,
-                    Layout: 3,
-                  })
-                }
-              >
-                <img src={multiview2} alt="2" />
-              </Button>
-              <Button
-                variant={mv.properties.layout == 10 ? 'primary' : 'secondary'}
-                onClick={() =>
-                  this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewPropertiesSetV8Command', {
-                    MultiviewIndex: mvIndex,
-                    Mask: 1,
-                    Layout: 10,
-                  })
-                }
-              >
-                <img src={multiview3} alt="3" />
-              </Button>
-              <Button
-                variant={mv.properties.layout == 5 ? 'primary' : 'secondary'}
-                onClick={() =>
-                  this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewPropertiesSetV8Command', {
-                    MultiviewIndex: mvIndex,
-                    Mask: 1,
-                    Layout: 5,
-                  })
-                }
-              >
-                <img src={multiview4} alt="4" />
-              </Button>
-            </ButtonGroup>
-          </Row>
-        ) : (
-          ''
-        )}
+
+        <div className="multiViewProperties">
+          {info.canChangeLayout ? (
+            <div className="layout">
+              <div className="atem-heading">View Layout</div>
+              {this.renderChangeLayout(info, mv.properties)}
+            </div>
+          ) : (
+            ''
+          )}
+          {info.supportsVuMeters ? (
+            <div className="vu-meters">
+              <div className="atem-heading">Audio Meters</div>
+              {this.renderVuControls(mv.properties)}
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+
         <div className="multiViewGrid">{this.renderContent(info, mv)}</div>
       </Container>
+    )
+  }
+}
+
+class LayoutButton extends React.Component<
+  {
+    sendCommand: SendCommandStrict
+    mvIndex: number
+    current: LibAtemEnums.MultiViewLayoutV8
+    target: LibAtemEnums.MultiViewLayoutV8
+  },
+  { hover: boolean }
+> {
+  constructor(props: LayoutButton['props']) {
+    super(props)
+
+    this.state = {
+      hover: false,
+    }
+
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+  }
+
+  onMouseEnter() {
+    this.setState({ hover: true })
+  }
+  onMouseLeave() {
+    this.setState({ hover: false })
+  }
+
+  render() {
+    const { current, target, mvIndex } = this.props
+    return (
+      <Button
+        className="multiViewLayoutButton"
+        bsPrefix="multiViewLayoutButton"
+        variant={current === target ? 'primary' : 'secondary'}
+        onClick={() =>
+          this.props.sendCommand('LibAtem.Commands.Settings.Multiview.MultiviewPropertiesSetV8Command', {
+            MultiviewIndex: mvIndex,
+            Mask: LibAtemCommands.Settings_Multiview_MultiviewPropertiesSetV8Command_MaskFlags.Layout,
+            Layout: target,
+          })
+        }
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      >
+        <LayoutIcon layout={target} isCurrent={current === target} isHover={this.state.hover} />
+      </Button>
+    )
+  }
+}
+
+class VuMeterIcon extends React.PureComponent<{ isCurrent: boolean }> {
+  render() {
+    const { isCurrent } = this.props
+    const colB = isCurrent ? '#ffffff' : '#848484'
+    const colM = isCurrent ? '#acacac' : '#7d7d7d'
+    const colT = isCurrent ? '#717171' : '#6e6e6e'
+    return (
+      <svg width="22px" height="22px" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <rect id="l5" x="5" y="2" width="5" height="2" fill={colT} />
+          <rect id="r5" x="12" y="2" width="5" height="2" fill={colT} />
+          <rect id="l4" x="5" y="6" width="5" height="2" fill={colT} />
+          <rect id="r4" x="12" y="6" width="5" height="2" fill={colT} />
+
+          <rect id="l3" x="5" y="10" width="5" height="2" fill={colM} />
+          <rect id="r3" x="12" y="10" width="5" height="2" fill={colM} />
+
+          <rect id="l2" x="5" y="14" width="5" height="2" fill={colB} />
+          <rect id="r2" x="12" y="14" width="5" height="2" fill={colB} />
+          <rect id="l1" x="5" y="18" width="5" height="2" fill={colB} />
+          <rect id="r1" x="12" y="18" width="5" height="2" fill={colB} />
+        </g>
+      </svg>
+    )
+  }
+}
+
+class SafeAreaIcon extends React.PureComponent<{ isCurrent: boolean }> {
+  render() {
+    const { isCurrent } = this.props
+    const col = isCurrent ? '#ffffff' : '#848484'
+    return (
+      <svg width="22px" height="22px" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <rect
+            id="outer"
+            x="3"
+            y="3"
+            width="16"
+            height="16"
+            rx="1"
+            ry="1"
+            stroke-width="1.3"
+            stroke={col}
+            fill="none"
+          />
+
+          <rect id="top" x="10" y="1" width="2" height="5" fill={col} />
+          <rect id="bottom" x="10" y="16" width="2" height="5" fill={col} />
+          <rect id="left" x="1" y="10" width="5" height="2" fill={col} />
+          <rect id="right" x="16" y="10" width="5" height="2" fill={col} />
+        </g>
+      </svg>
     )
   }
 }
